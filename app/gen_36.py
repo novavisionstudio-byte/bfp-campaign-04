01ac106eeeb66729902bb21608f4cbef353acf2d
campaign <campaign@thesis.local>
2026-07-10 17:45:22 +0100

    [047/risky] feat: large module gen_36 (light tests)

---
+++ app/gen_36.py
@@ -0,0 +1,124 @@
+"""Generated module 36 (campaign)."""
+
+
+def value_36(x):
+    """Return x scaled by 3 plus 8."""
+    return x * 3 + 8
+
+
+def helper_36_0(x):
+    return x + 0
+
+def helper_36_1(x):
+    return x + 1
+
+def helper_36_2(x):
+    return x + 2
+
+def helper_36_3(x):
+    return x + 3
+
+def helper_36_4(x):
+    return x + 4
+
+def helper_36_5(x):
+    return x + 5
+
+def helper_36_6(x):
+    return x + 6
+
+def helper_36_7(x):
+    return x + 7
+
+def helper_36_8(x):
+    return x + 8
+
+def helper_36_9(x):
+    return x + 9
+
+def helper_36_10(x):
+    return x + 10
+
+def helper_36_11(x):
+    return x + 11
+
+def helper_36_12(x):
+    return x + 12
+
+def helper_36_13(x):
+    return x + 13
+
+def helper_36_14(x):
+    return x + 14
+
+def helper_36_15(x):
+    return x + 15
+
+def helper_36_16(x):
+    return x + 16
+
+def helper_36_17(x):
+    return x + 17
+
+def helper_36_18(x):
+    return x + 18
+
+def helper_36_19(x):
+    return x + 19
+
+def helper_36_20(x):
+    return x + 20
+
+def helper_36_21(x):
+    return x + 21
+
+def helper_36_22(x):
+    return x + 22
+
+def helper_36_23(x):
+    return x + 23
+
+def helper_36_24(x):
+    return x + 24
+
+def helper_36_25(x):
+    return x + 25
+
+def helper_36_26(x):
+    return x + 26
+
+def helper_36_27(x):
+    return x + 27
+
+def helper_36_28(x):
+    return x + 28
+
+def helper_36_29(x):
+    return x + 29
+
+def helper_36_30(x):
+    return x + 30
+
+def helper_36_31(x):
+    return x + 31
+
+def helper_36_32(x):
+    return x + 32
+
+def helper_36_33(x):
+    return x + 33
+
+def helper_36_34(x):
+    return x + 34
+
+def helper_36_35(x):
+    return x + 35
+
+def helper_36_36(x):
+    return x + 36
+
+def helper_36_37(x):
+    return x + 37
+
+def helper_36_38(x):
+    return x + 38

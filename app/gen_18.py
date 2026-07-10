6f4cc580d79a3c605db878b80c170f2320bfcf59
campaign <campaign@thesis.local>
2026-07-10 17:10:43 +0100

    [018/risky] feat: large module gen_18 (light tests)

---
+++ app/gen_18.py
@@ -0,0 +1,184 @@
+"""Generated module 18 (campaign)."""
+
+
+def value_18(x):
+    """Return x scaled by 6 plus 2."""
+    return x * 6 + 2
+
+
+def helper_18_0(x):
+    return x + 0
+
+def helper_18_1(x):
+    return x + 1
+
+def helper_18_2(x):
+    return x + 2
+
+def helper_18_3(x):
+    return x + 3
+
+def helper_18_4(x):
+    return x + 4
+
+def helper_18_5(x):
+    return x + 5
+
+def helper_18_6(x):
+    return x + 6
+
+def helper_18_7(x):
+    return x + 7
+
+def helper_18_8(x):
+    return x + 8
+
+def helper_18_9(x):
+    return x + 9
+
+def helper_18_10(x):
+    return x + 10
+
+def helper_18_11(x):
+    return x + 11
+
+def helper_18_12(x):
+    return x + 12
+
+def helper_18_13(x):
+    return x + 13
+
+def helper_18_14(x):
+    return x + 14
+
+def helper_18_15(x):
+    return x + 15
+
+def helper_18_16(x):
+    return x + 16
+
+def helper_18_17(x):
+    return x + 17
+
+def helper_18_18(x):
+    return x + 18
+
+def helper_18_19(x):
+    return x + 19
+
+def helper_18_20(x):
+    return x + 20
+
+def helper_18_21(x):
+    return x + 21
+
+def helper_18_22(x):
+    return x + 22
+
+def helper_18_23(x):
+    return x + 23
+
+def helper_18_24(x):
+    return x + 24
+
+def helper_18_25(x):
+    return x + 25
+
+def helper_18_26(x):
+    return x + 26
+
+def helper_18_27(x):
+    return x + 27
+
+def helper_18_28(x):
+    return x + 28
+
+def helper_18_29(x):
+    return x + 29
+
+def helper_18_30(x):
+    return x + 30
+
+def helper_18_31(x):
+    return x + 31
+
+def helper_18_32(x):
+    return x + 32
+
+def helper_18_33(x):
+    return x + 33
+
+def helper_18_34(x):
+    return x + 34
+
+def helper_18_35(x):
+    return x + 35
+
+def helper_18_36(x):
+    return x + 36
+
+def helper_18_37(x):
+    return x + 37
+
+def helper_18_38(x):
+    return x + 38
+
+def helper_18_39(x):
+    return x + 39
+
+def helper_18_40(x):
+    return x + 40
+
+def helper_18_41(x):
+    return x + 41
+
+def helper_18_42(x):
+    return x + 42
+
+def helper_18_43(x):
+    return x + 43
+
+def helper_18_44(x):
+    return x + 44
+
+def helper_18_45(x):
+    return x + 45
+
+def helper_18_46(x):
+    return x + 46
+
+def helper_18_47(x):
+    return x + 47
+
+def helper_18_48(x):
+    return x + 48
+
+def helper_18_49(x):
+    return x + 49
+
+def helper_18_50(x):
+    return x + 50
+
+def helper_18_51(x):
+    return x + 51
+
+def helper_18_52(x):
+    return x + 52
+
+def helper_18_53(x):
+    return x + 53
+
+def helper_18_54(x):
+    return x + 54
+
+def helper_18_55(x):
+    return x + 55
+
+def helper_18_56(x):
+    return x + 56
+
+def helper_18_57(x):
+    return x + 57
+
+def helper_18_58(x):
+    return x + 58
